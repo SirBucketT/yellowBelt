6b3de6b3560cf76a0ffe7f61fe7f9dc7d5d50565
SirBucketT <tkpriva@icloud.com>
2024-11-13 18:30:05 +0100

    kata 6 done

--- Mini-Kata 6/Program.cs
+++ Mini-Kata 6/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] enemies = {"Goblin", "Orc", "troll" };
+        Console.WriteLine($"Enemies:");
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(enemies[i]);
+        }Console.WriteLine();
+        
+        string[] playerInventory = { "Sword", "Shield", "Potion" };
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(playerInventory[i]);
+        }Console.WriteLine();
+        
+        List<string> inventoryList = new List<string>(playerInventory);
+        
+        inventoryList.Add("Helmet");
+        
+        playerInventory = inventoryList.ToArray();
+        
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine(playerInventory[i]);
+        }Console.WriteLine();
     }
 }
