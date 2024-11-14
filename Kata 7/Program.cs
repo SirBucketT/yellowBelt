edbc4dd6d011c8326cbae0a15860bb3cdbc1b3e2
SirBucketT <tkpriva@icloud.com>
2024-11-14 16:07:23 +0100

    kata 7 progress

---
+++ Kata 7/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Kata_7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    class Player
+    {
+        string name;
+        int Health;
+        int Level;
+        int Experience;
+        int Damage;
+
+        void Attack(int damage)
+        {
+            
+        }
+
+        void GainExperience(int exp)
+        {
+            
+        }
+        
+    }
+
+    class Enemy
+    {
+
+    }
+
+}
