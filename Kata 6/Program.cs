08038c4f464a10f25140a02f3961ae3a667729a9
SirBucketT <tkpriva@icloud.com>
2024-11-14 14:06:20 +0100

    kata 6 completed

--- Kata 6/Program.cs
+++ Kata 6/Program.cs
@@ -6,6 +6,42 @@ class Program
     {
        string[] enemylist = { "Goblin", "Orc", "Troll", "Skeleton", "Dragon" };
 
-    }
+       Console.WriteLine("Enemies:");
+       
+       foreach (var i in enemylist)
+       {
+           Console.WriteLine(i);
+       }
 
+       string[] playerInventory = { "Sword", "Shield", "Potion" };
+       
+       Console.WriteLine();
+       Console.WriteLine("Player Inventory:");
+
+       foreach (var i in playerInventory)
+       {
+           Console.WriteLine(i);
+       }
+
+       Console.WriteLine();
+
+       List<string> playerInventoryList = new List<string>(playerInventory);
+       
+       playerInventoryList.Add("Helmet");
+       playerInventoryList.Add("Armor");
+       playerInventoryList.Remove("Potion");
+       
+       playerInventory = playerInventoryList.ToArray();
+       
+       Console.WriteLine("Updated Inventory:");
+       
+       foreach (var i in playerInventory)
+       {
+           Console.WriteLine(i);
+       }
+
+       Console.WriteLine();
+       Console.WriteLine($"Total Items in Inventory: {playerInventoryList.Count}");
+    }
 }
+
