adf971d445e10d56030f24d1d034d8e41a40e466
SirBucketT <tkpriva@icloud.com>
2024-11-15 13:44:29 +0100

    kata 7 done

--- Kata 7/Program.cs
+++ Kata 7/Program.cs
@@ -4,32 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Player player = new Player();
+        Enemy enemy = new Enemy();
+
+        player.Attack(damage:50);
+        enemy.TakeDamage(damage:50);
     }
 
     class Player
     {
-        string name;
-        int Health;
-        int Level;
-        int Experience;
-        int Damage;
+        public string name = "Arin";
+        public int Health = 100;
+        public int Level = 1;
+        public int Experience = 50;
+        public int Damage = 50;
 
-        void Attack(int damage)
+        public void Attack(int damage)
         {
-            
+            Console.WriteLine($"{name} attacks the enemy and deals {damage} damage");
         }
 
-        void GainExperience(int exp)
+        public void GainExperience(int exp)
         {
-            
+            Experience += exp;
+            Console.WriteLine($"{name} Gained {exp} Experience.");
         }
         
     }
 
     class Enemy
     {
+        public string Type = "Orc";
+        public int Health = 100;
 
+        public void TakeDamage(int damage)
+        {
+            if (damage <= 0)
+            {
+                Console.WriteLine($"{Type} has been defeated");
+                damage = 0;
+                return;
+            }
+            Health -= damage;
+            Console.WriteLine($"{Type} takes {damage} damage. Remaining Health: {Health}");
+        }
     }
 
 }
