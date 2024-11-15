0e8cd8d2947b4a0c2f65a8b8d0ac15c697b5168d
SirBucketT <tkpriva@icloud.com>
2024-11-15 18:31:31 +0100

    kata 8 progress

---
+++ Kata 8/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Kata_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    class Player
+    {
+        private int Level;
+        private int Experience;
+
+        public int Health {get; private set;}
+
+        public int Level
+        {
+            get { return Level; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Level cannot be negative.");
+                    level = value;
+                }
+            }
+        }
+    }
+}
