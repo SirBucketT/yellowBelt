55f9394846f0dd4efa0e086554705b67b71a6217
SirBucketT <tkpriva@icloud.com>
2024-11-18 12:23:29 +0100

    kata 8

--- Kata 8/Program.cs
+++ Kata 8/Program.cs
@@ -5,17 +5,52 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Player player = new Player();
+        player.GainExperience(50);
+        Console.WriteLine("pLAYER GAINED 60 EXP");
+        player.GainExperience(60);
+        
+        
+        if (player.Experience >= 100)
+        {
+            Console.WriteLine($"Congratulations! You leveled up to Level {player.Level}.");
+        }
     }
 
     class Player
     {
-        private int Level;
-        private int Experience;
+        private int _level;
+        private int _experience;
         private int Health;
+        
+        public int health { get; private set; }
 
-        public int level
+        public int GainExperience(int exp)
         {
-            get { return Level; }
+            _experience += exp;
+            
+            if (_experience >= 100)
+            {
+                LevelUp();
+            }
+            else
+            {
+                Console.WriteLine($"{_experience} experience points.");
+            }
+
+            return _experience;
+        }
+
+        private void LevelUp()
+        {
+            Level++;
+            Experience = 0;
+            Console.WriteLine($"Congratulations! You level {Level}.");
+        }
+
+        public int Level
+        {
+            get { return _level; }
             set
             {
                 if (value < 0)
@@ -23,13 +58,13 @@ class Program
                     throw new ArgumentException("Level cannot be negative.");
                 }
                 
-                Level = value;
+                _level = value;
             }
         }
         
-        public int experience
+        public int Experience
         {
-            get { return Experience; }
+            get { return _experience; }
             set
             {
                 if (value < 0)
@@ -37,7 +72,7 @@ class Program
                     throw new ArgumentException("Exp cannot be negative.");
                 }
                 
-                Experience = value;
+                _experience = value;
             }
         }
     }
